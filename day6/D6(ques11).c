392d134a0884c32d2da35690c888d899c4507a13
--get <--get>
2025-10-07 23:41:53 +0530

    chore(day6): add D6(ques11).c

---
+++ day6/D6(ques11).c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int num;
+    printf("Enter an integer: ");
+    if (scanf("%d", &num) != 1) {
+        fprintf(stderr, "Invalid input\n");
+        return 1;
+    }
+    if (num % 2 == 0) {
+        printf("%d is even\n", num);
+    } else {
+        printf("%d is odd\n", num);
+    }
+    return 0;
+}
