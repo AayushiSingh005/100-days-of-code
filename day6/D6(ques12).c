e69a9c30b26873f1f993250c2485dc725e88499e
--get <--get>
2025-10-07 23:44:52 +0530

    chore(day6): add D6(ques12).c

---
+++ day6/D6(ques12).c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int num;
+    printf("Enter an integer: ");
+    if (scanf("%d", &num) != 1) {
+        fprintf(stderr, "Invalid input\n");
+        return 1;
+    }
+    if (num > 0) {
+        printf("Positive\n");
+    } else if (num < 0) {
+        printf("Negative\n");
+    } else {
+        printf("Zero\n");
+    }
+    return 0;
+}
