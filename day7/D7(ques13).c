ed444e4f620fc204bb7c347b7fc5377d81dd2ccc
--get <--get>
2025-10-07 23:48:10 +0530

    chore(day7): add D7(ques13).c

---
+++ day7/D7(ques13).c
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int year;
+    printf("Enter a year: ");
+    if (scanf("%d", &year) != 1) {
+        fprintf(stderr, "Invalid input\n");
+        return 1;
+    }
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                printf("Leap year\n");
+            } else {
+                printf("Not a leap year\n");
+            }
+        } else {
+            printf("Leap year\n");
+        }
+    } else {
+        printf("Not a leap year\n");
+    }
+    return 0;
+}
