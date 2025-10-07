5b881d21b0756cf49c74bd101978e30691eaf44b
--get <--get>
2025-10-07 23:40:02 +0530

    chore(day5): add D5(ques10).c

---
+++ day5/D5(ques10).c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int seconds, hours, minutes, remaining_seconds;
+    printf("Enter time in seconds: ");
+    if (scanf("%d", &seconds) != 1) {
+        fprintf(stderr, "Invalid input\n");
+        return 1;
+    }
+    hours = seconds / 3600;
+    minutes = (seconds % 3600) / 60;
+    remaining_seconds = seconds % 60;
+    printf("%d:%02d:%02d\n", hours, minutes, remaining_seconds);
+    return 0;
+}
