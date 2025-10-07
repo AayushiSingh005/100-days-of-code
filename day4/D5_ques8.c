f579d9cdf2550bc523cbc4cb84b96ef18e67ae00
--get <--get>
2025-10-07 23:28:20 +0530

    chore(day4): add D5_ques8.c

---
+++ day4/D5_ques8.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    int n, sum = 0;
+    printf("Enter a number: ");
+    if (scanf("%d", &n) != 1) {
+        fprintf(stderr, "Invalid input\n");
+        return 1;
+    }
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    printf("Sum=%d\n", sum);
+    return 0;
+}
