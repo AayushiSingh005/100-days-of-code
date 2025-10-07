78bd4ee709f34056ecbee164d22ee3445f5ed4f9
--get <--get>
2025-10-07 23:51:01 +0530

    chore(day7): add D7(ques14).c

---
+++ day7/D7(ques14).c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+int main(void) {
+    char ch;
+    printf("Enter a character: ");
+    if (scanf(" %c", &ch) != 1) {
+        fprintf(stderr, "Invalid input\n");
+        return 1;
+    }
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        printf("Vowel\n");
+    } else {
+        printf("Consonant\n");
+    }
+    return 0;
+}
