ac85995496242378abda15bc3e70fff6a9471ace
Aayushi Singh <92842294+AayushiSingh005@users.noreply.github.com>
2025-11-04 09:38:22 +0530

    Update D26(ques51).c

---
+++ day26/D26(ques51).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, k;
+    int n = 5;  // You can change n for a larger pattern
+
+    for (i = 1; i <= n; i++) {
+        // Print leading spaces
+        for (j = i; j < n; j++) {
+            printf(" ");
+        }
+        // Print numbers
+        for (k = n - i + 1; k <= n; k++) {
+            printf("%d", k);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
