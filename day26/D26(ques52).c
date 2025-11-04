4b671f47e7fce405091c155f977ed96b81bffe73
Aayushi Singh <92842294+AayushiSingh005@users.noreply.github.com>
2025-11-04 09:39:06 +0530

    Update D26(ques52).c

---
+++ day26/D26(ques52).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Printing the given pattern exactly
+    printf("*\n\n");
+    printf("*\n*\n*\n\n");
+    printf("*\n*\n*\n*\n*\n\n");
+    printf("*\n*\n*\n\n");
+    printf("*\n");
+
+    return 0;
+}
